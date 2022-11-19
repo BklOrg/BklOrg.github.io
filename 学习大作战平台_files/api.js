5cec5e7e7a3b3901f1a8992af060e22d0b77ee92
binklings <96730988+binklings@users.noreply.github.com>
2022-11-19 14:24:47 +0800

    Update api.js

--- 学习大作战平台_files/api.js
+++ 学习大作战平台_files/api.js
@@ -1,55 +1,55 @@
 let API_URL = {
-    setLiangshiBuy: "/index/setLiangshiBuy",
-    addZaixianShichang: "/index/addZaixianShichang",
-    setFangchengmiTime: "/index/setFangchengmiTime",
-    getFangchengmiTime: "/index/getFangchengmiTime",
-    questionTrueFalseList: "/question/questionTrueFalseList",
-    getQuestionStaticList: "/question/getQuestionStaticList",
-    submitQuestion: "/question/submitQuestion",
-    getQuestionDetail: "/question/getQuestionDetail",
-    getQuestionStatus: "/question/getQuestionStatus",
-    submitQuestionBefore: "/question/submitQuestionBefore",
-    changeQuestionSwitch: "/question/changeQuestionSwitch",
-    addQuestion: "/question/addQuestion",
-    getQuestionList: "/question/getQuestionList",
-    getBankList: "/question/getBankList",
-    submitBank: "/question/submitBank",
-    ifHaveGongji: "/index/ifHaveGongji",
-    ifHaveZhencha: "/index/ifHaveZhencha",
-    jiesuanZhandou: "/index/jiesuanZhandou",
-    xiugai: "/index/xiugai",
-    zhengbing: "/index/zhengbing",
-    zhiyuan: "/index/zhiyuan",
-    gongji: "/index/gongji",
-    zhencha: "/index/zhencha",
-    getMy: "/index/getMy",
-    resetPwd: "/index/resetPwd",
-    getUserInfo: "/user/getUserInfo",
-    change_pwd: "/user/change_pwd",
-    uploadStudents: "/user/uploadStudents",
-    addLiangshi: "/index/addLiangshi",
-    get_gongfang_list: "/index/get_gongfang_list", //获取攻防记录日志
-    get_support_list: "/index/get_support_list", //获取支援记录日志
+    setLiangshiBuy: "http://114.116.245.61:4000/index/setLiangshiBuy",
+    addZaixianShichang: "http://114.116.245.61:4000/index/addZaixianShichang",
+    setFangchengmiTime: "http://114.116.245.61:4000/index/setFangchengmiTime",
+    getFangchengmiTime: "http://114.116.245.61:4000/index/getFangchengmiTime",
+    questionTrueFalseList: "http://114.116.245.61:4000/question/questionTrueFalseList",
+    getQuestionStaticList: "http://114.116.245.61:4000/question/getQuestionStaticList",
+    submitQuestion: "http://114.116.245.61:4000/question/submitQuestion",
+    getQuestionDetail: "http://114.116.245.61:4000/question/getQuestionDetail",
+    getQuestionStatus: "http://114.116.245.61:4000/question/getQuestionStatus",
+    submitQuestionBefore: "http://114.116.245.61:4000/question/submitQuestionBefore",
+    changeQuestionSwitch: "http://114.116.245.61:4000/question/changeQuestionSwitch",
+    addQuestion: "http://114.116.245.61:4000/question/addQuestion",
+    getQuestionList: "http://114.116.245.61:4000/question/getQuestionList",
+    getBankList: "http://114.116.245.61:4000/question/getBankList",
+    submitBank: "http://114.116.245.61:4000/question/submitBank",
+    ifHaveGongji: "http://114.116.245.61:4000/index/ifHaveGongji",
+    ifHaveZhencha: "http://114.116.245.61:4000/index/ifHaveZhencha",
+    jiesuanZhandou: "http://114.116.245.61:4000/index/jiesuanZhandou",
+    xiugai: "http://114.116.245.61:4000/index/xiugai",
+    zhengbing: "http://114.116.245.61:4000/index/zhengbing",
+    zhiyuan: "http://114.116.245.61:4000/index/zhiyuan",
+    gongji: "http://114.116.245.61:4000/index/gongji",
+    zhencha: "http://114.116.245.61:4000/index/zhencha",
+    getMy: "http://114.116.245.61:4000/index/getMy",
+    resetPwd: "http://114.116.245.61:4000/index/resetPwd",
+    getUserInfo: "http://114.116.245.61:4000/user/getUserInfo",
+    change_pwd: "http://114.116.245.61:4000/user/change_pwd",
+    uploadStudents: "http://114.116.245.61:4000/user/uploadStudents",
+    addLiangshi: "http://114.116.245.61:4000/index/addLiangshi",
+    get_gongfang_list: "http://114.116.245.61:4000/index/get_gongfang_list", //获取攻防记录日志
+    get_support_list: "http://114.116.245.61:4000/index/get_support_list", //获取支援记录日志
 //    注册接口
-    register: "/user/register",
+    register: "http://114.116.245.61:4000/user/register",
 //    登录接口
     login: "http://114.116.245.61:4000/user/login",
     //提交
-    submit_data: '/index/submit',
+    submit_data: 'http://114.116.245.61:4000/index/submit',
 //    获取列表
-    get_list: '/index/get_list',
+    get_list: 'http://114.116.245.61:4000/index/get_list',
 //  删除
-    del_info: '/index/del_info',
+    del_info: 'http://114.116.245.61:4000/index/del_info',
 
     //创建接收人
-    create_reciver: "/reciver/create",
+    create_reciver: "http://114.116.245.61:4000/reciver/create",
     //获取接收人列表
-    get_reciver_list: "/reciver/get_list",
+    get_reciver_list: "http://114.116.245.61:4000/reciver/get_list",
     //删除接收人
-    del_reciver: "/reciver/del",
+    del_reciver: "http://114.116.245.61:4000/reciver/del",
     
     //    测试接口
-    test: "/index/test",
+    test: "http://114.116.245.61:4000/index/test",
     
 }
 
